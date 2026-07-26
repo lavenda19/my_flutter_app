3429876640daa5708ff7155ea95c2ec6272ee66e
lavenda19 <mercylavenda2005@gmail.com>
2026-07-26 19:38:41 +0300

    Improved recipe details screen design

--- lib/views/recipe_details_screen.dart
+++ lib/views/recipe_details_screen.dart
@@ -1,35 +1,26 @@
 import 'package:flutter/material.dart';
 import '../model/recipe.dart';
 
-
 class RecipeDetailsScreen extends StatefulWidget {
-
   final Recipe recipe;
-
-
   const RecipeDetailsScreen({
     super.key,
     required this.recipe,
   });
 
-
   @override
   State<RecipeDetailsScreen> createState() =>
       _RecipeDetailsScreenState();
-
 }
-
-
 class _RecipeDetailsScreenState extends State<RecipeDetailsScreen> {
-
-
   @override
   Widget build(BuildContext context) {
 
     return Scaffold(
 
-      appBar: AppBar(
+      backgroundColor: Colors.grey[100],
 
+      appBar: AppBar(
         title: Text(
           widget.recipe.name,
           style: const TextStyle(
@@ -37,163 +28,104 @@ class _RecipeDetailsScreenState extends State<RecipeDetailsScreen> {
             fontWeight: FontWeight.bold,
           ),
         ),
-
         backgroundColor: Colors.deepOrange,
-
         centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {
+              setState(() {
+                widget.recipe.isFavorite =
+                !widget.recipe.isFavorite;
+              });
+            },
 
+            icon: Icon(
+              widget.recipe.isFavorite
+                  ? Icons.favorite
+                  : Icons.favorite_border,
+              color: Colors.white,
+            ),
+          ),
+        ],
       ),
 
-
       body: SingleChildScrollView(
-
         child: Column(
-
           crossAxisAlignment: CrossAxisAlignment.start,
-
           children: [
+            ClipRRect(
+              borderRadius: const BorderRadius.only(
+                bottomLeft: Radius.circular(30),
+                bottomRight: Radius.circular(30),
+              ),
 
-
-            Image.network(
-
-              widget.recipe.image,
-
-              width: double.infinity,
-
-              height: 250,
-
-              fit: BoxFit.cover,
-
+              child: Image.network(
+                widget.recipe.image,
+                width: double.infinity,
+                height: 250,
+                fit: BoxFit.cover,
+              ),
             ),
 
-
-
             Padding(
-
               padding: const EdgeInsets.all(16),
-
               child: Column(
-
                 crossAxisAlignment: CrossAxisAlignment.start,
 
                 children: [
-
-
                   Text(
-
                     widget.recipe.name,
-
                     style: const TextStyle(
-
                       fontSize: 28,
-
                       fontWeight: FontWeight.bold,
-
                     ),
-
                   ),
-
-
 
                   const SizedBox(height: 10),
-
-
-
                   Text(
-
                     "⏱ Cooking Time: ${widget.recipe.time}",
-
                     style: const TextStyle(
-
                       fontSize: 16,
-
                     ),
-
                   ),
 
-
-
                   const SizedBox(height: 20),
-
-
-
                   const Text(
-
                     "Ingredients",
-
                     style: TextStyle(
-
                       fontSize: 22,
-
                       fontWeight: FontWeight.bold,
-
                     ),
-
                   ),
-
-
 
                   Text(
-
                     widget.recipe.ingredients,
-
                     style: const TextStyle(
-
                       fontSize: 16,
-
                     ),
-
                   ),
-
-
 
                   const SizedBox(height: 20),
-
-
-
                   const Text(
-
                     "Instructions",
-
                     style: TextStyle(
-
                       fontSize: 22,
-
                       fontWeight: FontWeight.bold,
-
                     ),
-
                   ),
-
-
 
                   Text(
-
                     widget.recipe.instructions,
-
                     style: const TextStyle(
-
                       fontSize: 16,
-
                     ),
-
                   ),
-
-
                 ],
-
               ),
-
             ),
-
           ],
-
         ),
-
       ),
-
     );
-
   }
-
 }
