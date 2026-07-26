e50868d8a94afc8f07177d37213c343f34472e02
lavenda19 <mercylavenda2005@gmail.com>
2026-07-26 16:00:29 +0300

    Added recipe model and recipe data

---
+++ lib/config/recipe_data.dart
@@ -0,0 +1,13 @@
+import '../model/recipe.dart';
+
+List<Recipe> recipes = [
+
+  Recipe(
+    name: "Chicken Curry",
+    ingredients: "Chicken, Onion, Tomatoes, Spices",
+    instructions: "Cook chicken with spices and serve hot.",
+    time: "30 minutes",
+    image: "https://images.unsplash.com/photo-1603894584373-5ac82b2ae398",
+  ),
+
+];
---
+++ lib/model/recipe.dart
@@ -0,0 +1,17 @@
+class Recipe {
+
+  String name;
+  String ingredients;
+  String instructions;
+  String time;
+  String image;
+
+  Recipe({
+    required this.name,
+    required this.ingredients,
+    required this.instructions,
+    required this.time,
+    required this.image,
+  });
+
+}
